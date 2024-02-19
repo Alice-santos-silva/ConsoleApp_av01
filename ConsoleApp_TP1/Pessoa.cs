620f5be31644cb77a1c0a15020f2527a577ce218
Alice Santos <alicesantossilva345@gmail.com>
2024-02-19 15:41:40 -0300

    Primerio commit

---
+++ ConsoleApp_TP1/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_TP1
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+        private string cpf;
+
+        public string Nome { get => nome; set => nome = value; }
+        public int Idade { get => idade; set => idade = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+
+        public Pessoa(string nome, int idade, string cpf) {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Cpf = cpf;
+        }
+    }
+}
---
+++ ConsoleApp_TP1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp_TP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            Pessoa p1 = new Pessoa("Alice", 21, "123456789");
+            Console.WriteLine(p1.Nome);
+            Console.WriteLine(p1.Idade);
+            Console.WriteLine(p1.Cpf);
+
+            while (true)
+            {
+                Console.WriteLine("1 - nova pessoa");
+                Console.WriteLine("2 - listar pessoas");
+                Console.WriteLine("3 - sair");
+                Console.WriteLine("(aperte qualquer tecla para sair.)");
+                string escolha = Console.ReadLine();
+                int escolhaInt = int.Parse(escolha);
+                
+
+                if (escolhaInt == 1)
+                {
+                    Console.WriteLine("NOVA PESSOA");
+                    Console.WriteLine("Nome: ");
+                    string novoNome = Console.ReadLine();
+                    Console.WriteLine("Idade: ");
+                    string novaIdade = Console.ReadLine();
+                    int idadeInt = int.Parse(novaIdade);
+                    Console.WriteLine("CPF: ");
+                    string novoCpf = Console.ReadLine();
+                    pessoas.Add(new Pessoa(novoNome, idadeInt, novoCpf));
+                }
+                else if(escolhaInt == 2)
+                {
+                    foreach(Pessoa pessoa in  pessoas)
+                    {
+                        Console.WriteLine(pessoa.Nome + "-" + pessoa.Idade + "-" + pessoa.Cpf);
+                    }
+                }
+                else if(escolhaInt == 3)
+                {
+                    Console.WriteLine("Programa encerrado.");
+                    break;
+                }
+            }
+        }
+    }
+}
